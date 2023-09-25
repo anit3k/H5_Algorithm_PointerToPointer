f76157c8444b5ecd675859f6f97091a2468f208a
anit3k <steffen.halberg@gmail.com>
2023-09-25 11:03:04 +0200

    Add project files.

---
+++ H5_Algorithm_PointerToPointer/Main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+	int x = 666; // declare value of 66 in x
+	int *ptr1 = &x; // declare pointer 1 and point address to the x
+	int** ptr2 = &ptr1; // declare new pointer (2) and set the pointer to pointer 1
+	printf("The value is before: %d\n", x); // write out value
+	printf("The value is before: %d\n", **ptr2); // write out value
+
+	**ptr2 = 66; // set new value
+	printf("The value is after: %d\n", x);
+	printf("The value is after: %d\n", **ptr2);
+}
